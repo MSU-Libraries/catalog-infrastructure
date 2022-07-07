618e68d322308734ad65b789e48eabfe8cf25f5e
Megan Schanz <schanzme@msu.edu>
2022-07-07 13:43:56 -0400

    Trying to force run of user data on node replacement

--- terraform/module/node/main.tf
+++ terraform/module/node/main.tf
@@ -93,10 +93,11 @@ resource "aws_instance" "node_instance" {
     }
   )
 
-  lifecycle {
-    ignore_changes = [
-      user_data,
-    ]
+  # TODO Commenting out to try and run user data on node replacement
+  #lifecycle {
+  #  ignore_changes = [
+  #    user_data,
+  #  ]
    #prevent_destroy = true # TODO We'll add this back in once we're keeping servers up consistently
-  }
+  #}
 }
