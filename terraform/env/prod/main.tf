5e24879740732629a8e617e3182b87fe357982ad
Nathan Collins <colli372@msu.edu>
2022-08-05 16:08:33 -0400

    infrastructure: increase capacity of AWS instance

--- terraform/env/prod/main.tf
+++ terraform/env/prod/main.tf
@@ -34,7 +34,7 @@ module "catalog" {
       server_name = "catalog-a"
       aws_availability_zone = "a"
       aws_ami = "ami-052efd3df9dad4825"
-      aws_instance_type = "t3a.large"
+      aws_instance_type = "t3a.xlarge"
       aws_root_block_size = 64
       private_ip = "10.1.1.10"
       subnet_cidr = "10.1.1.0/24"
@@ -43,7 +43,7 @@ module "catalog" {
       server_name = "catalog-b"
       aws_availability_zone = "b"
       aws_ami = "ami-052efd3df9dad4825"
-      aws_instance_type = "t3a.large"
+      aws_instance_type = "t3a.xlarge"
       aws_root_block_size = 64
       private_ip = "10.1.2.10"
       subnet_cidr = "10.1.2.0/24"
@@ -52,7 +52,7 @@ module "catalog" {
       server_name = "catalog-c"
       aws_availability_zone = "c"
       aws_ami = "ami-052efd3df9dad4825"
-      aws_instance_type = "t3a.large"
+      aws_instance_type = "t3a.xlarge"
       aws_root_block_size = 64
       private_ip = "10.1.3.10"
       subnet_cidr = "10.1.3.0/24"
