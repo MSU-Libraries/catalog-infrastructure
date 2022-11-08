f53c171319fba97142449c7dc931ff5e58ee7240
Nathan Collins <colli372@msu.edu>
2022-11-08 14:41:01 -0500

    Blanks for ncpa plugins

---
+++ configure-playbook/roles/ncpa-plugins/files/check_docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# TODO check docker cluster state
+
+# TODO check appropriate stacks are deployed
+
+# TODO check appropriate replicas are running
+
+# TODO check containers have been running for longer than 30 seconds
+
+# TODO check if there are unknown containers running with given prefix (e.g. catalog-beta-strangeservice)
+
+exit 0
