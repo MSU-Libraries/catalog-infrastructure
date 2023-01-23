acb7dc5e76858c8986540a094141a803853bd536
Nathan Collins <colli372@msu.edu>
2023-01-23 17:16:38 -0500

    Remove 'production' from NCPA msg; add some TODO comments

--- configure-playbook/roles/ncpa-plugins/files/plugins/check_zk.sh
+++ configure-playbook/roles/ncpa-plugins/files/plugins/check_zk.sh
@@ -2,6 +2,8 @@
 
 # TODO verify each node's Zookeeper instance is okay (ruok)
 
-# TODO verify cluster has 1 leader and 2 followers
+# TODO verify cluster has 1 leader and 2 non-leaders (from each node, if possible)
+
+# TODO verify solr collections' status.json are identical from each node, if possible
 
 exit 0
