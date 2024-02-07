1937c02621a7d8fa9d08f2abe5c11f40a7dec03b
Megan Schanz <schanzme@msu.edu>
2024-02-07 11:09:13 -0500

    Add helper script for locating OAI files

---
+++ configure-playbook/roles/deploy-helper-scripts/files/auto_pc-locate-oai.bash
@@ -0,0 +1,22 @@
+_pc-locate-oai()
+{
+    shopt -s nullglob
+    COMPREPLY=()
+    CURRENT="${COMP_WORDS[$COMP_CWORD]}"
+    OAI_DIR="/mnt/shared/oai"
+    # Change directory to avoid cases where user is currently in an unreadable
+    # directory (e.g. just after a sudo) which would cause `find` to throw errors
+    cd /
+
+    if [[ $COMP_CWORD -eq 2 ]]; then
+        # Get all the environment directory names in OAI_DIR
+        readarray -d '' DEPLOY_ENVS < <(find ${OAI_DIR} \
+            -maxdepth 1 -mindepth 1 -type d \
+            -exec realpath --relative-to ${OAI_DIR} {} \;)
+
+        mapfile -t COMPREPLY < <(compgen -W "${DEPLOY_ENVS[*]}" -- "$CURRENT")
+    fi
+    shopt -u nullglob
+    return 0
+}
+complete -F _pc-locate-oai pc-locate-oai
