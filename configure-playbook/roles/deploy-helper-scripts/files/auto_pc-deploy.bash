7dde179febd5acc2b23c70599c55e947b1adaf5e
Nathan Collins <colli372@msu.edu>
2024-04-04 12:56:37 -0400

    PC-974 Surpress find restore CWD msgs; remove tabauto dir chg

--- configure-playbook/roles/deploy-helper-scripts/files/auto_pc-deploy.bash
+++ configure-playbook/roles/deploy-helper-scripts/files/auto_pc-deploy.bash
@@ -5,27 +5,24 @@ _pc-deploy()
     CURRENT="${COMP_WORDS[$COMP_CWORD]}"
     PREV="${COMP_WORDS[$COMP_CWORD-1]}"
     DEPLOY_DIR="/home/deploy"
-    # Change directory to avoid cases where user is currently in an unreadable
-    # directory (e.g. just after a sudo) which would cause `find` to throw errors
-    [[ -r $PWD ]] || cd /
 
     if [[ $COMP_CWORD -eq 1 ]]; then
         # Get all the environment directory names in DEPLOY_DIR
         readarray -d '' DEPLOY_ENVS < <(find ${DEPLOY_DIR} \
             -maxdepth 1 -mindepth 1 '(' -name "catalog-*" -o \
             -name "devel-*" -o -name "core-stacks" -o -name "review-*" ')' \
-            -exec realpath --relative-to ${DEPLOY_DIR} {} \;)
+            -exec realpath --relative-to ${DEPLOY_DIR} {} \; 2>/dev/null)
         
         mapfile -t COMPREPLY < <(compgen -W "${DEPLOY_ENVS[*]}" -- "$CURRENT")
     
     elif [[ $COMP_CWORD -eq 2 ]]; then
         # Finds all compose files in the environment directory selected
         readarray -d '' STACK_CONFS < <(find ${DEPLOY_DIR}/"${PREV}"/docker-compose.*.yml \
-            -exec realpath --relative-to ${DEPLOY_DIR}/"${PREV}" {} \;)
+            -exec realpath --relative-to ${DEPLOY_DIR}/"${PREV}" {} \; 2>/dev/null)
         
         # Finds just the names of the stacks (i.e solr-cloud or vufind)
         readarray -d '' STACKS < <(find ${DEPLOY_DIR}/"${PREV}"/docker-compose.*.yml \
-            -exec sh -c 'T="${0#*.}"; echo "${T%.yml}";' {}  \;)
+            -exec sh -c 'T="${0#*.}"; echo "${T%.yml}";' {}  \; 2>/dev/null)
        
         # Combine the two arrays for the final output of suggestions
         ALL=("${STACK_CONFS[@]}" "${STACKS[@]}")
