3c86302056325d402c38e4b31f195db643c5da18
Nathan Collins <colli372@msu.edu>
2024-06-12 15:13:33 -0400

    Terraform rename modules
    In prep for having multiple clusters

--- terraform/env/prod/main.tf
+++ terraform/env/prod/main.tf
@@ -6,20 +6,28 @@ terraform {
     }
 }
 
-module "mail" {
-  source = "../../module/mail_smtp"
+module "shared" {
+  source = "../../module/shared"
   aws_region = "us-east-1"
   mail_instance = "catalog-prod"
 }
 
-module "catalog" {
-  source = "../../module/catalog"
+moved {
+    from = module.mail
+    to   = module.shared
+}
+
+module "cluster" {
+  source = "../../module/cluster"
   cluster_name = "catalog"
   aws_region = "us-east-1"
   vpc_cidr = "10.1.0.0/16"
-  smtp_host = module.mail.smtp_host
-  smtp_user = module.mail.smtp_username
-  smtp_password = module.mail.smtp_password
+  #cluster_cidr = "10.1.1.10/22" # TODO change this value somehow
+  #domain = "aws.lib.msu.edu"
+  #zone_id = "Z0159018169CCNUQINNQG"
+  smtp_host = module.shared.smtp_host
+  smtp_user = module.shared.smtp_username
+  smtp_password = module.shared.smtp_password
   net_allow_inbound_ssh = [
     "0.0.0.0/0",
   ]
@@ -60,79 +68,84 @@ module "catalog" {
   }
 }
 
+moved {
+    from = module.catalog
+    to   = module.cluster
+}
+
 output "aws_region" {
   description = "AWS region being used"
-  value       = module.catalog.aws_region
+  value       = module.cluster.aws_region
 }
 
 output "dnschallenge_key_id" {
   description = "DNS challenge key id"
-  value       = module.catalog.dnschallenge_key_id
+  value       = module.cluster.dnschallenge_key_id
 }
 
 output "dnschallenge_key_secret" {
   description = "DNS chanllege key secret"
-  value       = module.catalog.dnschallenge_key_secret
+  value       = module.cluster.dnschallenge_key_secret
   sensitive   = true
 }
 
 output "catalog_a_instance_id" {
   description = "Instance ID (catalog-1)"
-  value = module.catalog.instance_ids[0]
+  value = module.cluster.instance_ids[0]
 }
 
 output "catalog_a_fqdn" {
   description = "FQDN (catalog-1)"
-  value = module.catalog.fqdns[0]
+  value = module.cluster.fqdns[0]
 }
 
 output "catalog_a_public_ip" {
   description = "Public IP (catalog-1)"
-  value = module.catalog.public_ips[0]
+  value = module.cluster.public_ips[0]
 }
 
 output "catalog_a_efs_hostname" {
   description = "AWS EFS hostname (catalog-1)"
-  value = module.catalog.efs_hostnames[0]
+  value = module.cluster.efs_hostnames[0]
 }
 
 output "catalog_b_instance_id" {
   description = "Instance ID (catalog-2)"
-  value = module.catalog.instance_ids[1]
+  value = module.cluster.instance_ids[1]
 }
 
 output "catalog_b_fqdn" {
   description = "FQDB (catalog-2)"
-  value = module.catalog.fqdns[1]
+  value = module.cluster.fqdns[1]
 }
 
 output "catalog_b_public_ip" {
   description = "Public IP (catalog-2)"
-  value = module.catalog.public_ips[1]
+  value = module.cluster.public_ips[1]
 }
 
 output "catalog_b_efs_hostname" {
   description = "AWS EFS hostname (catalog-2)"
-  value = module.catalog.efs_hostnames[1]
+  value = module.cluster.efs_hostnames[1]
 }
 
 output "catalog_c_instance_id" {
   description = "Instance ID (catalog-3)"
-  value = module.catalog.instance_ids[2]
+  value = module.cluster.instance_ids[2]
 }
 
 output "catalog_c_fqdn" {
   description = "FQDN (catalog-3)"
-  value = module.catalog.fqdns[2]
+  value = module.cluster.fqdns[2]
 }
 
 output "catalog_c_public_ip" {
   description = "Public IP (catalog-3)"
-  value = module.catalog.public_ips[2]
+  value = module.cluster.public_ips[2]
 }
 
 output "catalog_c_efs_hostname" {
   description = "AWS EFS hostname (catalog-3)"
-  value = module.catalog.efs_hostnames[2]
+  value = module.cluster.efs_hostnames[2]
 }
 
