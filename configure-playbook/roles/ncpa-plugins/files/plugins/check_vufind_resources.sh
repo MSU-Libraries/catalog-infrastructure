1395f3fa551390cf89f8c377ead8618e2714c646
Megan Schanz <schanzme@msu.edu>
2024-06-20 14:30:18 -0400

    PC-1059 Add ncpa script for checking memory and cpu usage of the vufind container

---
+++ configure-playbook/roles/ncpa-plugins/files/plugins/check_vufind_resources.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+## Script to check the Memory and CPU usage of the VuFind container
+## which helps identify when bot attacks are occurring.
+
+# Prepend sudo to a command if user is not in docker group
+docker_sudo() {
+    if ! groups | grep -qw docker; then sudo "$@";
+    else "$@"; fi
+}
+
+if [[ -z "$1" ]]; then
+    echo "UNKNOWN: You must provide a deployment name (e.g. catalog-beta) as the first argument."
+    exit 3
+fi
+
+# Stack deployment name (e.g. catalog-beta, devel-nathan)
+DEPLOYMENT="$1"
+
+is_main() {
+    [[ "$DEPLOYMENT" == "catalog"* ]]
+    return $?
+}
+
+# Verify that the numerical stat output for the given resource is less than the
+# provided warning and critical values and exit with the appropriate code.
+verify_resource() {
+    # Ex: MemPerc, CPUPerc (found in docker stats --no-stream --no-trunc --format json [container])
+    RESOURCE="$1"
+    WARN="$2" # Numerical cut off when a warning should be triggered (i.e. 10)
+    CRIT="$3" # Numerical cut off when a critical error should be triggered (i.e. 15)
+
+    STAT=$( docker stats --no-stream --format "{{.${RESOURCE}}}" "$(docker ps -q -f name="${DEPLOYMENT}"-catalog_catalog)")
+
+    if [[ -z "$STAT" ]]; then
+        echo "UNKNOWN: Could not get ${RESOURCE} in VuFind container for ${DEPLOYMENT}"
+        exit 3
+    fi
+    
+    NORMALIZED_STAT=$(printf '%.*f\n' 0 "${STAT//\%/}")
+    MESSAGE="${RESOURCE} for ${DEPLOYMENT} VuFind container is ${STAT}"
+
+    # shellcheck disable=SC2004 # check is not correctly realizing that the $ is required when in the if-statement
+    if (( $NORMALIZED_STAT > $CRIT )); then
+        echo "CRITICAL: ${MESSAGE}"
+        exit 2
+    elif (( $NORMALIZED_STAT > $WARN )); then
+        echo "WARNING: ${MESSAGE}"
+        exit 1
+    else
+        echo "OK: ${MESSAGE}"
+    fi
+}
+
+verify_resource "MemPerc" 70 85
+verify_resource "CPUPerc" 70 85
+
+exit 0
