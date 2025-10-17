71dad6232e9482c8e830f083ff4bc6629e76b56b
Megan Schanz <schanzme@msu.edu>
2025-10-17 14:31:19 -0400

    PC-1551 Create helper script to create missing solr collections and aliases

---
+++ configure-playbook/roles/deploy-helper-scripts/files/auto_pc-solr.bash
@@ -0,0 +1,28 @@
+_pc-locate-hlm()
+{
+    shopt -s nullglob
+    COMPREPLY=()
+    CURRENT="${COMP_WORDS[$COMP_CWORD]}"
+    LOCAL_DIR="/mnt/shared/local"
+    # Change directory to avoid cases where user is currently in an unreadable
+    # directory (e.g. just after a sudo) which would cause `find` to throw errors
+    cd /
+
+    # TODO -- tab completion doesn't work if flags are passed first
+    if [[ $COMP_CWORD -eq 1 ]]; then
+        # Get all the environment directory names in LOCAL_DIR
+        readarray -d '' DEPLOY_ENVS < <(find ${LOCAL_DIR} \
+            -maxdepth 1 -mindepth 1 -type d \
+            -exec realpath --relative-to ${LOCAL_DIR} {} \;)
+
+        mapfile -t COMPREPLY < <(compgen -W "${DEPLOY_ENVS[*]}" -- "$CURRENT")
+    fi
+
+    # TODO -- adding this makes tab completion stop working
+    # (the options display correctly, but can't tab complete to them)
+    #COMPREPLY+=(-p --pattern -v --verbose --debug)
+
+    shopt -u nullglob
+    return 0
+}
+complete -F _pc-solr pc-solr
