9b297e1ce19017158d8666e37d83356740a5a708
Megan Schanz <schanzme@msu.edu>
2025-12-10 14:07:00 -0500

    PC-1570 Update user_data to fix heredoc within if-statement

--- terraform/module/node/user_data.sh
+++ terraform/module/node/user_data.sh
@@ -3,47 +3,49 @@
 # shellcheck disable=SC2154
 
 ### Setup root user which is used by CI to run the configure-playbook
-sudo -Hsu root -- <<-ROOT
+sudo -Hsu root -- <<ROOT
     install -d -m 0700 /root/.ssh
     echo "${root_public_key}" >> /root/.ssh/authorized_keys
     chmod 0600 /root/.ssh/authorized_keys
 ROOT
 
+### Install packages required for mail configuration
+sudo -Hs -- <<POSTSETUP
+    debconf-set-selections <<< "postfix postfix/mailname string ${mail_name}"
+    debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
+    DEBIAN_FRONTEND=noninteractive \
+    apt update && \
+    apt install mailutils postfix libsasl2-modules -y
+    sleep 5
+    umask 077
+    echo "[${smtp_host}]:587 ${smtp_user}:${smtp_password}" > /etc/postfix/sasl_passwd
+    postmap hash:/etc/postfix/sasl_passwd
+    postconf -e "relayhost = [${smtp_host}]:587" \
+    "smtp_tls_CAfile = /etc/ssl/certs/ca-certificates.crt" \
+    "myhostname = ${mail_name}" \
+    "append_dot_mydomain = yes" \
+    "smtp_sasl_auth_enable = yes" \
+    "smtp_sasl_security_options = noanonymous" \
+    "smtp_sasl_password_maps = hash:/etc/postfix/sasl_passwd" \
+    "smtp_use_tls = yes" \
+    "smtp_tls_security_level = encrypt" \
+    "smtp_tls_note_starttls_offer = yes" \
+    "inet_interfaces = loopback-only"
+    systemctl restart postfix
+POSTSETUP
 
 ### Create ansible user if a key is provided,
 ### so that the ubuntu-setup-playbook can be run
-if [[ -n ${ansible_public_key} ]]; then
-    sudo useradd -m -u 4444 -s /bin/bash ansible
-    echo "ansible ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers
-    sudo -Hsu ansible -- <<-ANSIBLE
+if [[ -z ${ansible_public_key} ]]; then
+    exit 0
+fi
+useradd -m -u 4444 -s /bin/bash ansible
+echo "ansible ALL=(ALL) NOPASSWD: ALL" | tee -a /etc/sudoers
+sudo -Hsu ansible -- <<ANSIBLE
     cd
     umask 077
     mkdir .ssh
     echo "${ansible_public_key}" >> .ssh/authorized_keys
 ANSIBLE
-fi
 
-### Install packages required for mail configuration 
-sudo -Hs -- <<-POSTSETUP
-	debconf-set-selections <<< "postfix postfix/mailname string ${mail_name}"
-	debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
-	DEBIAN_FRONTEND=noninteractive \
-	apt update && \
-	apt install mailutils postfix libsasl2-modules -y
-	sleep 5
-	umask 077
-	echo "[${smtp_host}]:587 ${smtp_user}:${smtp_password}" > /etc/postfix/sasl_passwd
-	postmap hash:/etc/postfix/sasl_passwd
-	postconf -e "relayhost = [${smtp_host}]:587" \
-	"smtp_tls_CAfile = /etc/ssl/certs/ca-certificates.crt" \
-	"myhostname = ${mail_name}" \
-	"append_dot_mydomain = yes" \
-	"smtp_sasl_auth_enable = yes" \
-	"smtp_sasl_security_options = noanonymous" \
-	"smtp_sasl_password_maps = hash:/etc/postfix/sasl_passwd" \
-	"smtp_use_tls = yes" \
-	"smtp_tls_security_level = encrypt" \
-	"smtp_tls_note_starttls_offer = yes" \
-	"inet_interfaces = loopback-only"
-	systemctl restart postfix
-POSTSETUP
+# NOTE: if you add more steps, be aware of the previous if-statement's return
